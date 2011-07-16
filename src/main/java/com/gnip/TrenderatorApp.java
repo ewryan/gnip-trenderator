4dbcd82250c0c94b529af082e0d6fdeb0acebfd0
ewryan <ewryan@gmail.com>
2011-07-16 13:00:30 -0600

    cleaning up test data

--- src/main/java/com/gnip/TrenderatorApp.java
+++ src/main/java/com/gnip/TrenderatorApp.java
@@ -5,7 +5,10 @@ import com.gnip.consumers.FileStreamConsumer;
 public class TrenderatorApp {
 
     public static void main(String[] args) throws Exception {
-//        new HTTPStreamConsumer().getStream(new TrendingStreamHandler());
+        //Uncomment the following line and supply credentials in HTTPStreamConsumer if you want to use a Gnip Data Collector.
+        //new HTTPStreamConsumer().getStream(new TrendingStreamHandler());
+
+        //Use the following line for local testing
         new FileStreamConsumer().getStream(new TrendingStreamHandler());
     }
 
